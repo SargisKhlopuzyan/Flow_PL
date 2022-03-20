168d2cdd394042865490d3849b6973014b90949e
Sargis Khlopuzyan <sargis.khlopuzyan@gmail.com>
2022-03-20 14:31:55 +0400

    Flow tutorial

---
+++ app/src/main/java/com/sargis/khlopuzyan/flow_pl/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.sargis.khlopuzyan.flow_pl
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sargis.khlopuzyan.flow_pl.ui.theme.Flow_PLTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Flow_PLTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Flow_PLTheme {
+        Greeting("Android")
+    }
+}
